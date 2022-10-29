6fb20ca61180fed76ee5fafd3088417a3c6f2b7c
Nx0520 <nxfulgor@gmail.com>
2022-10-29 18:57:32 -0500

    first commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  Post,
+  Get,
+  Delete,
+  Body,
+  Param,
+  ParseIntPipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
+    return this.usersService.getUser(id);
+  }
+
+  @Post()
+  createUser(@Body() newUser: CreateUserDto) {
+    return this.usersService.createUser(newUser);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.deleteUser(id);
+  }
+}
